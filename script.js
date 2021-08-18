b5cfe322634437215fcd827e0a8f467bb9dcbb8f
Freaksta <Matthew_Furniss@yahoo.com>
2021-08-18 21:02:14 +1000

    built rps function

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-File creation
+let computerResult = null;
+let num = null;
+
+function computerPlay() {
+  const num = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+  console.log(num);
+
+  if (num == 1) {
+    computerResult = "Rock";
+  } else if (num == 2) {
+    computerResult = "Paper";
+  } else {
+    computerResult = "Scissors";
+  }
+  return computerResult;
+}
